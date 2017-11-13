8e8621093afce10c54f58cfedca19314f57d643a
mayur5 <mayur.relekar@wowlabz.com>
2017-11-13 21:40:39 +0530

    added ropsten network to truffle

--- truffle.js
+++ truffle.js
@@ -4,6 +4,11 @@ module.exports = {
       host: "localhost",
       port: 8545,
       network_id: "*" // Match any network id
+    },
+    ropsten: {
+      host: "localhost",
+      port: 8545,
+      network_id: "3" // Match any network id
     }
   }
 };
